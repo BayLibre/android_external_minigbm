2e385831ed43726c8149410d338b03d26a41a898
Juston Li <justonli@google.com>
2025-11-18 11:55:41 -0800

    xe: Query for PXP protected content support
    Use the query pxp status ioctl to check if the driver supports and has
    PXP enabled.
    
    Note we ignore the status field as we want to check whether PXP is
    supported not whether its active at the time of query.
    
    Bug=b/461785087
    Test=L1 playback
    
    Change-Id: Ib2f51ac05ad7df5822ffadcd6333db0d5a4df196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/minigbm/+/7164600
    Reviewed-by: Ryan Neph <ryanneph@google.com>
    Tested-by: Juston Li <justonli@google.com>
    Commit-Queue: Juston Li <justonli@google.com>

--- xe.c
+++ xe.c
@@ -312,6 +312,27 @@ static int xe_align_dimensions(struct bo *bo, uint32_t format, uint32_t tiling, 
 	return 0;
 }
 
+static void xe_query_pxp_support(struct driver *drv, struct xe_device *xe)
+{
+	struct drm_xe_query_pxp_status pxp_status = {0};
+	struct drm_xe_device_query query = {
+		.extensions = 0,
+		.query = DRM_XE_DEVICE_QUERY_PXP_STATUS,
+		.size = sizeof(struct drm_xe_query_pxp_status),
+		.data = (uintptr_t)&pxp_status,
+	};
+
+	/* Ignore the transient status, the query will return an error if PXP
+	 * is not supported or disabled due to a fatal error during init.
+	 */
+	if (drmIoctl(drv->fd, DRM_IOCTL_XE_DEVICE_QUERY, &query) == 0) {
+		if (pxp_status.supported_session_types & 1u << DRM_XE_PXP_TYPE_HWDRM)
+			xe->has_hw_protection = 1;
+	} else {
+		drv_logi("Xe PXP not supported or disabled.");
+	}
+}
+
 static bool xe_query_config(struct driver *drv, struct xe_device *xe)
 {
 	struct drm_xe_device_query query = {
@@ -392,15 +413,14 @@ static int xe_init(struct driver *drv)
 
 	xe_query_config(drv, xe);
 
+	xe_query_pxp_support(drv, xe);
+
 	/* must call before xe->graphics_version is used anywhere else */
 	xe_info_from_device_id(xe);
 
 	xe_get_modifier_order(xe);
 
-	/* Xe still don't have support for protected content */
-	if (xe->graphics_version >= 12)
-		xe->has_hw_protection = 0;
-	else if (xe->graphics_version < 12) {
+	if (xe->graphics_version < 12) {
 		drv_loge("Xe driver is not supported on your platform: 0x%x\n", xe->device_id);
 		return -errno;
 	}
